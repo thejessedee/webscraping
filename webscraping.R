8e740d547ac36250900f6bdfe1fa1597abf18664
thejessedee <64216111+thejessedee@users.noreply.github.com>
2022-09-18 20:39:36 -0500

    Add files via upload
    It is great when packages change how they approach things... RTweet changed... and this is the new way to get the info we will need.

--- webscraping.R
+++ webscraping.R
@@ -19,41 +19,38 @@ data <- data[2:443, 1:5]
 #Rename variables 
 names(data) <- c("first.name", "last.name", "handle", "district", "party")
 
-#Let's remove the '@' symbol from the twitter handles so we can do more 
+#Creating a variable for matching dataframes later (twitterhandle) 
 data <- data %>%
-  mutate(handle = gsub("@", "", handle)) 
+  mutate(screen_name = gsub("@", "", handle)) 
 
-#Creating a new dataframe that we will use in the next step 
 tweet_holder <- as.data.frame(NULL) 
 
-#Running all 442 twitter handles through our program and getting 20 tweets for each 
-#Presently you have to run this through three times in order to obtain all the tweets. 
-#However, you have to do this at one hour intervals. Because while you can get a tweets fast, 
-#Twitter only allows you to get 180 pulls per hour (iirc). So, you can only do 1-180, 
-#but you may be able to pull more tweets. Check the rules as they change every now and then. 
+#This is the code to run through this. However, you will need to figure out 
+#which accounts are not valid... and the program will stop. Thus, 
+#you will need to start and stop this a couple of times to figure out
+#which ones are good and then break around those. 
 
-#1-180
-for(i in 1:180){ 
-  handles <- twitter_url_remover(data$handle[i]) 
-  tweets <- get_timeline(handles, n=100) 
-  tweet_holder <- rbind(tweet_holder, tweets) 
-  print(i)
+
+for(i in 1:180){
+  timeline <- get_timeline(data$screen_name[i], n=10) 
+  bound <- cbind(timeline, users_data(timeline)[, c("id", "id_str", "name", "screen_name")]) 
+  tweet_holder <- rbind(tweet_holder, bound) 
+  print(i) 
 }
 
-#181-360
-for(i in 181:360){ 
-  handles <- twitter_url_remover(data$handle[i]) 
-  tweets <- get_timeline(handles, n=100) 
-  tweet_holder <- rbind(tweet_holder, tweets) 
-  print(i)
+for(i in 181:360){
+  timeline <- get_timeline(data$screen_name[i], n=10) 
+  bound <- cbind(timeline, users_data(timeline)[, c("id", "id_str", "name", "screen_name")]) 
+  tweet_holder <- rbind(tweet_holder, bound) 
+  print(i) 
 }
 
-#361-442
-for(i in 361:442){ 
-  handles <- twitter_url_remover(data$handle[i]) 
-  tweets <- get_timeline(handles, n=100) 
-  tweet_holder <- rbind(tweet_holder, tweets) 
-  print(i)
+for(i in 361:442){
+  timeline <- get_timeline(data$screen_name[i], n=10) 
+  bound <- cbind(timeline, users_data(timeline)[, c("id", "id_str", "name", "screen_name")]) 
+  tweet_holder <- rbind(tweet_holder, bound) 
+  print(i) 
 }
+
 
 
